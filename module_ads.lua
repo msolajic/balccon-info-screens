5047b192dcf6d9a0cf7e2e199fff4d53e04faad4
marko <msolajic@gmail.com>
2016-09-09 21:56:56 +0200

    Signed-off-by: marko <msolajic@gmail.com>

--- module_ads.lua
+++ module_ads.lua
@@ -52,7 +52,7 @@ end
 function M.prepare(options)
     local image 
     image, image_idx = utils.cycled(imglist, image_idx)
-    return 15, image
+    return 10, image
 end
 
 function M.run(duration, image, fn)
